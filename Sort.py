e20c1893ad8546dc5b342b67799e1d36e67fba56
Sanal-T <sanalt2024@gmail.com>
2025-05-04 22:41:43 +0530

    Added merge sort

--- Sort.py
+++ Sort.py
@@ -27,13 +27,45 @@ def quick_sort(arr):
         return quick_sort(less) + [pivot] + quick_sort(greater)
 
 
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        L = arr[:mid]
+        R = arr[mid:]
+
+        merge_sort(L)
+        merge_sort(R)
+
+        i = j = k = 0
+
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+
 # Main program
 print("Select sorting algorithm:")
 print("1. Bubble Sort")
 print("2. Insertion Sort")
 print("3. Quick Sort")
+print("4. Merge Sort")
 
-choice = int(input("Enter choice (1/2/3): "))
+choice = int(input("Enter choice (1/2/3/4): "))
 
 arr = list(map(int, input("Enter numbers separated by space: ").split()))
 
@@ -46,5 +78,8 @@ elif choice == 2:
 elif choice == 3:
     arr = quick_sort(arr)
     print("Sorted array (Quick Sort):", arr)
+elif choice == 4:
+    merge_sort(arr)
+    print("Sorted array (Merge Sort):", arr)
 else:
     print("Invalid choice!")
